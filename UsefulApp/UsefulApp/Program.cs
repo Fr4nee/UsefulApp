7e765491fa0633f47f771d58b818bd99667f7004
Fr4nee <65635671+Fr4nee@users.noreply.github.com>
2021-07-06 13:40:25 -0300

    fran1

--- UsefulApp/UsefulApp/Program.cs
+++ UsefulApp/UsefulApp/Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
+using System.Linq;
 
 namespace UsefulApp
 {
     class Program
     {
         static int op = 0;
+
+
+        static string[,] Calendar = new string[7, 7]
+        {
+            {"Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"},
+            { null,null,null,null,null,null,"1)"},
+            { "2)","3)","4)","5)","6)","7)","8)"},
+            { "9)","10)","11)","12)","13)","14)","15)"},
+            { "16)","17)","18)","19)","20)","21)","22)"},
+            { "23)","24)","25)","26)","27)","28)","29)"},
+            { "30)","31)",null,null,null,null,null},
+        };
+
+
+
         static void Main(string[] args)
         {
-
             App();
-        }
+        }     
         static void App()
         {
-            do
-            {
+            do         
                 MainMenu();
-            }
             while (op != 5);
         }
         static void MainMenu()
@@ -40,19 +53,21 @@ namespace UsefulApp
             {
                 case 1:
                     Console.Clear();
-                    Console.WriteLine("entro1");
+                    Console.WriteLine("entro 1");
+                    Test();
                     break;
                 case 2:
                     Console.Clear();
-                    Console.WriteLine("entro2");
+                    Console.WriteLine("entro 2");
+                    IntroduceData();
                     break;
                 case 3:
                     Console.Clear();
-                    Console.WriteLine("entro3");
+                    Console.WriteLine("entro 3");
                     break;
                 case 4:
                     Console.Clear();
-                    Console.WriteLine("entro4");
+                    Console.WriteLine("entro 4");
                     break;
                 case 5:
                     Console.Clear();
@@ -60,12 +75,12 @@ namespace UsefulApp
                     break;
 
                 default:
+                    Console.Clear();
+                    Console.WriteLine("Select the correct option...");
                     break;
             }
             return op;
-
         }
-
         public static int ExitApp()
         {
             string exit;
@@ -77,6 +92,66 @@ namespace UsefulApp
                 op = 0;
 
             return op;
+        }
+
+
+        static void Test()
+        {         
+            Console.WriteLine();
+            for (int fi = 0; fi < Calendar.GetLength(0); fi++)
+            {
+                Console.Write("|");
+                for (int ci = 0; ci < Calendar.GetLength(1); ci++)
+                {
+                    Console.Write($"{Calendar[fi, ci],-13}|");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+
+        static void IntroduceData()
+        {
+
+            string dato;
+
+            string dayWeek;
+            string day;
+
+            Test();
+
+            for (int f = 0; f < Calendar.GetLength(0); f++)
+            {
+                Console.WriteLine("Ingrese dia de la semana:");
+                dayWeek = Console.ReadLine();
+                if (Calendar[0, f] == dayWeek)
+                {
+                    for (int c = 0; c < Calendar.GetLength(1); c++)
+                    {
+                        Console.WriteLine("Ingrese dia:");
+                        day = Console.ReadLine();
+
+                        if (Calendar[f, c] == day)
+                        {
+                            dato = Console.ReadLine();
+                            Calendar[f, c] = dato;
+                        }
+                        break;
+                    }
+                }
+            }
+
+
+            
+            
+
+
+
+
+
+
+
         }
 
     }
